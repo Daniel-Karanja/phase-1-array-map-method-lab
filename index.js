0493257cc384ea5dd0a31549e1760d5aae485d98
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-09 10:14:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((word) => {
+    let k = word.split(" ");
+    let s = "";
+    k.forEach((str) => {
+      s = s + str[0].toUpperCase() + str.slice(1, str.length) + " ";
+    });
+    return s.trim();
+  });
+};
+
+console.log(titleCased());
